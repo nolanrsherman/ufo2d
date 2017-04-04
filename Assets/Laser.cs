f7ae02ac0d0b9be4df67ede23a98c8138d7223ed
Nolan Sherman <nolanrsherman@gmail.com>
2017-04-04 16:22:07 -0500

    spaceship now shoots laser. Need to destroy laser after leaves game screen or collides with enemy/pickup

---
+++ Assets/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position = this.gameObject.transform.position + new Vector3 (0, speed, 0);
+	}
+
+	public void setSpeed(float speed){
+		this.speed = speed;
+	}
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,12 +6,22 @@ public class PlayerController : MonoBehaviour {
 
 	public Rigidbody2D rb;
 	public float speed;
+
+	public GameObject laserPrefab;
+	public float laserOffsetX1;
+	public float laserOffsetY1;
+	public float laserOffsetX2;
+	public float laserOffsetY2;
+
+	private bool mouseDown = false;
+
 	private int numberOfPickups;
 	private LevelManager levelmanager;
 
 
 	// Use this for initialization
 	void Start () {
+
 		rb = GetComponent<Rigidbody2D>();
 		rb.freezeRotation = true; //no rotation
 		levelmanager = GameObject.FindObjectOfType<LevelManager> ();
@@ -24,8 +34,23 @@ public class PlayerController : MonoBehaviour {
 		//float moveVertical = Input.GetAxis ("Vertical");// dont need this anymore
 		Vector2 movement = new Vector2 (moveHorizontal, 0);//0 vertical, we only want to move left to right
 		rb.AddForce(movement * speed);
+
+		onClick ();
 		CheckForWin ();
 
+	}
+
+	void onClick(){
+		//check for click or touch.
+		bool leftClick = Input.GetKeyDown(KeyCode.Mouse0);
+		if (leftClick) {
+			mouseDown = true;
+			Vector3 laserLocation1 = this.gameObject.transform.position + new Vector3 (laserOffsetX1, laserOffsetY1, 0);
+			Vector3 laserLocation2 = this.gameObject.transform.position + new Vector3 (laserOffsetX2, laserOffsetY2, 0);
+
+			Instantiate (laserPrefab, laserLocation1, new Quaternion(0f,0f,0f,0f));
+			Instantiate (laserPrefab, laserLocation2, new Quaternion(0f,0f,0f,0f));
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -33,14 +58,12 @@ public class PlayerController : MonoBehaviour {
 			other.gameObject.SetActive (false);
 			numberOfPickups--;
 		}
-		if (other.gameObject.CompareTag ("boundry")) {
-			
-		}
 	}
 
 	void CheckForWin() {
 		if (numberOfPickups == 0) {
+			
 			levelmanager.LoadWin ();
-		}
+		} 
 	}
 }
