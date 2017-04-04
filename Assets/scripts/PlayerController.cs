44f11acf8e389973404a15d0ffdcbf57f689e02b
Nolan Sherman <nolanrsherman@gmail.com>
2017-04-04 10:20:24 -0500

    Finished Menus from menu workshop led by Chase L.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour {
 
 	public Rigidbody2D rb;
 	public float speed;
+	private int numberOfPickups;
+	private LevelManager levelmanager;
 
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		levelmanager = GameObject.FindObjectOfType<LevelManager> ();
+		numberOfPickups = GameObject.FindGameObjectsWithTag ("pickup").Length;
 	}
 	
 	// Update is called once per frame
@@ -19,12 +23,20 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb.AddForce(movement * speed);
+		LoadNextLevel ();
 
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag("pickup") ) {
 			other.gameObject.SetActive (false);
+			numberOfPickups--;
+		}
+	}
+
+	void LoadNextLevel() {
+		if (numberOfPickups == 0) {
+			levelmanager.LoadWin ();
 		}
 	}
 }
